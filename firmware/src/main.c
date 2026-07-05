35368b7004fdf99a8809d4da9616fd061a455880
Ji Dong <ji_dong@steris.com>
2026-07-05 13:41:57 +0100

    link led to cs

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -5,12 +5,27 @@
 
 #include "bridge_ring.h"
 #include "spi_capture.h"
+#include "spi_mosi_sniffer.pio.h"
 #include "usb_stream.h"
 
 static bridge_ring_t bridge_ring;
 
 static void bridge_init_system_clock(void) {
     hard_assert(set_sys_clock_khz(160000u, true));
+}
+
+static void bridge_init_status_led(void) {
+#ifdef PICO_DEFAULT_LED_PIN
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    gpio_put(PICO_DEFAULT_LED_PIN, 0);
+#endif
+}
+
+static void bridge_update_status_led(void) {
+#ifdef PICO_DEFAULT_LED_PIN
+    gpio_put(PICO_DEFAULT_LED_PIN, !gpio_get(PICO_SPI_BRIDGE_CS_PIN));
+#endif
 }
 
 int main(void) {
@@ -23,12 +38,14 @@ int main(void) {
     });
 
     board_init();
+    bridge_init_status_led();
     tusb_init();
 
     while (true) {
         tud_task();
         spi_capture_poll();
         usb_stream_poll(&bridge_ring);
+        bridge_update_status_led();
         tight_loop_contents();
     }
 }
